062b6d24139c6b5fad634905bed6eb6943c29010
ArgonianVoter <FuckLindseyGraham@Tutamail.com>
2026-01-08 20:05:13 -0500

    Update New_YorkprocessEvents.js
    gotta put something.

--- America/New_YorkprocessEvents.js
+++ America/New_YorkprocessEvents.js
@@ -1,14 +1,20 @@
 export const ProccessEvents = [
   {
-    name: "Mr.Somebody Davids",
-    type: "weirdos",
-    contacts: ["555-5555", "whatever@gmail.com"],
-    websites: ["www.politician.gov"],
-    addresses: ["4123 main str, WA"]
+    name: "Ms.Governor Kathy Hochul",
+    type: "sponsors",
+    contacts: ["1-518-474-8390", "whatever@gmail.com"],
+    websites: ["https://www.governor.ny.gov/content/governor-contact-form"],
+    addresses: ["The Honorable Kathy Hochul\nGovernor of New York State\nNYS State Capitol Building\nAlbany, NY 12224"]
   },
   {
-    summary: "Test.",
-    type: "legalfight",
-    websites: ["www.stuff.com"]
+    name: "Safe for Kids Act S7694A",
+    type: "bill",
+    websites: ["https://www.nysenate.gov/legislation/bills/2023/S7694/amendment/A"],
+  },
+  {
+    summary: "NetChoice.",
+    type: "advocates_against",
+    websites: ["https://netchoice.org/netchoice-condemns-new-yorks-new-unconstitutional-internet-censorship-law/"]
   }
 ];
+
