1b4f6f693071c6465594794ef73e65608ff05b8c
ArgonianVoter <FuckLindseyGraham@Tutamail.com>
2026-01-08 19:08:34 -0500

    Add files via upload

---
+++ America/New_YorkprocessEvents.js
@@ -0,0 +1,14 @@
+export const ProccessEvents = [
+  {
+    name: "Mr.Somebody Davids",
+    type: "weirdos",
+    contacts: ["555-5555", "whatever@gmail.com"],
+    websites: ["www.politician.gov"],
+    addresses: ["4123 main str, WA"]
+  },
+  {
+    summary: "Test.",
+    type: "legalfight",
+    websites: ["www.stuff.com"]
+  }
+];
